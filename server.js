7170cebe2d4786bf6f515542fdc7fe48a491d5fd
Ezequiel Rubio <eze.rubio@gmail.com>
2016-10-15 00:37:51 +0200

    Add static files server

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 var express = require('express'),       // call express
 	app = express(),                 // define our app using express
 	bodyParser = require('body-parser'),
+	path = require('path'),
 	router = require('./src/router');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var port = process.env.PORT || 8080;        // set our port
 
